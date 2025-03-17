e9e843151fc5cae2ddc18dd85c02fce96600d971
Livia <livia.kelebercova@gmail.com>
2025-03-17 15:47:03 +0100

    Migracie, Seedery - cviko

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -4,25 +4,21 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB; 
+use Illuminate\Support\Facades\DB;
 
 class PostSeeder extends Seeder
 {
+    /**
+     * Run the database seeds.
+     */
     public function run(): void
     {
         DB::table('posts')->insert([
-            [
-                'title' => 'My first post',
-                'content' => 'My first content',
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-            [
-                'title' => 'My second post',
-                'content' => 'My second content',
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
+            ['title' => 'Prvý článok', 'content' => 'Obsah prvého článku', 
+            'user_id' => 1, 'category_id' => 1],
+            
+            ['title' => 'Druhý článok', 'content' => 'Obsah druhého článku', 
+            'user_id' => 2, 'category_id' => 2]
         ]);
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,33 +4,23 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB; 
-use Illuminate\Support\Facades\Hash; 
-use Illuminate\Support\Str; 
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
+    /**
+     * Run the database seeds.
+     */
     public function run(): void
     {
+        //
         DB::table('users')->insert([
-            [
-                'name' => 'Admin User',
-                'email' => 'admin@example.com',
-                'password' => Hash::make('password123'),
-                'email_verified_at' => now(),
-                'remember_token' => Str::random(10),
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-            [
-                'name' => 'Test User',
-                'email' => 'test@example.com',
-                'password' => Hash::make('password123'),
-                'email_verified_at' => now(),
-                'remember_token' => Str::random(10),
-                'created_at' => now(),
-                'updated_at' => now(),
-            ]
+            ['name' => 'Admin User', 'email' => 'admin@example.com', 
+            'password' => bcrypt('password'), 'role' => 'admin'],
+            
+            ['name' => 'Redactor User', 'email' => 'redactor@example.com', 
+            'password' => bcrypt('password'), 'role' => 'redactor']
         ]);
     }
 }
